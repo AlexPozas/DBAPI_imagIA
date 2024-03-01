15ea83fd443b542ceaccb3263fc41e1003f1e1fc
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2024-03-01 16:29:24 +0100

    pepe

--- src/main/java/cat/iesesteveterradas/dbapi/persistencia/taules/Peticions.java
+++ src/main/java/cat/iesesteveterradas/dbapi/persistencia/taules/Peticions.java
@@ -58,7 +58,8 @@ public class Peticions {
 
     public Peticions(JSONObject data) {
         JSONArray ja = new JSONArray(data.getJSONArray("images"));
-        this.user = UserManager.findUser(data.getString("user"));
+        System.out.println(data.getString("user"));
+        this.user = UserManager.findUser("admin");
         this.prompt = data.getString("prompt");
         this.model = ModelManager.findModelByName("llava");
 
